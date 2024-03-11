494bda1b4c2b655ef25770acd614e9fe73db7d68
Salim Hossain <salimh5757@gmail.com>
2024-03-11 17:13:05 +0600

    fix: auth slice

---
+++ src/features/auth/authApi.js
@@ -0,0 +1,27 @@
+import { apiSlice } from '../api/apiSlice';
+
+
+export const authApi = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        // endpoints
+        register: builder.mutation({
+            query: data => ({
+                url: '/register',
+                method: 'POST',
+                body: data
+            })
+        }),
+        login: builder.mutation({
+            query: data => ({
+                url: '/login',
+                method: 'POST',
+                body: data
+            })
+        }),
+    })
+});
+
+export const { 
+    useRegisterMutation, 
+    useLoginMutation
+ } = authApi;
